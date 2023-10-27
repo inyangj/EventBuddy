ec519e78da3852958617b61e559ba819be99b91a
inyangj <inyangjohnson01@gmail.com>
2023-10-27 14:06:43 +0100

    bug fix for registration

--- src/Components/modals/Register.jsx
+++ src/Components/modals/Register.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Input from "../Input";
 import Button from "../Button";
 import { useModal } from "../../contexts/ModalContext";
